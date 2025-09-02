cc5fbabdd13d6ef8d62179f69d1970a18abb3e94
logicaltomy <logicaltomy@gmail.com>
2025-09-02 16:55:22 -0400

    1.5.0

---
+++ usuarios-api/src/main/java/cl/condor/usuarios_api/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+// UsuarioRepository.java
+package cl.condor.usuarios_api.repository;
+
+import cl.condor.usuarios_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByRut(String rut);
+    Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByRut(String rut);
+    boolean existsByCorreo(String correo);
+}
