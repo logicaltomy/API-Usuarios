97f43508dbd678995d5e3fec873e260c8e6c0ec5
logicaltomy <logicaltomy@gmail.com>
2025-09-02 22:00:43 -0400

    1.13.0

---
+++ usuarios-api/src/main/java/cl/condor/usuarios_api/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package cl.condor.usuarios_api.controller;
+
+import cl.condor.usuarios_api.model.Usuario;
+import cl.condor.usuarios_api.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> lista = usuarioService.findAll();
+        if (lista.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(usuarioService.findById(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(201).body(usuarioService.save(usuario));
+    }
+}
