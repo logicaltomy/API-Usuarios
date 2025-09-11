bc540d74a1feb97ff25a1b31ec49b5b55d4705dd
logicaltomy <logicaltomy@gmail.com>
2025-09-11 18:42:23 -0300

    1.17.0 - "Restricciones de service para hacer save"

--- usuarios-api/src/main/java/cl/condor/usuarios_api/service/UsuarioService.java
+++ usuarios-api/src/main/java/cl/condor/usuarios_api/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package cl.condor.usuarios_api.service;
-
 import cl.condor.usuarios_api.model.Usuario;
+import cl.condor.usuarios_api.repository.EstadoRepository;
+import cl.condor.usuarios_api.repository.RegionRepository;
+import cl.condor.usuarios_api.repository.RolRepository;
 import cl.condor.usuarios_api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,15 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private RegionRepository regionRepository;
+
+    @Autowired
+    private RolRepository rolRepository;
+
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
@@ -24,7 +35,21 @@ public class UsuarioService {
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
     }
 
+    // Usuario depende de 3 tablas para su creación
+    // Depende de rol, estado, region
+    // hacer region como null?
+
     public Usuario save(Usuario usuario) {
+        if (usuario.getIdRegion() == null || !regionRepository.existsById(usuario.getIdRegion())) {
+            throw new RuntimeException("La región es obligatoria, no se puede guardar el Usuario");
+        }
+        if (usuario.getIdEstado() == null || !estadoRepository.existsById(usuario.getIdEstado())) {
+            throw new RuntimeException("Estado no encontrado, no se puede guardar el Usuario");
+        }
+        if (usuario.getIdRol() == null || !rolRepository.existsById(usuario.getIdRol())) {
+            throw new RuntimeException("Rol no encontrado, no se puede guardar el Usuario");
+        }
         return usuarioRepository.save(usuario);
     }
+
 }
